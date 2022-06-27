6b83d80ccadc3681537e88475fdaa9703a6ea575
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-06-27 17:00:39 +0200

    Update SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
+import axios from "axios";
 
 export default function SearchBar() {
+  let [city, setCity] = useState("");
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleWeather(response) {
+    console.log(response);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city.length > 0) {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a68381d4faf2a13b11b7dc8945964fc7&units=metric`;
+      axios.get(url).then(handleWeather);
+    } else {
+      alert("Enter a city please!");
+    }
+  }
+
   return (
     <div className="searchBar">
-      <form id="search-bar">
+      <form id="search-bar" onSubmit={handleSubmit}>
         <div className="row g-3 inputCityBar">
           <div className="col-auto">
             <i className="fa-solid fa-magnifying-glass" id="searchButton"></i>
@@ -15,6 +36,7 @@ export default function SearchBar() {
               id="inputCity"
               className="form-control"
               placeholder="Enter a city here"
+              onChange={changeCity}
             />
           </div>
         </div>
