9ea3962b2b0dbd2821d564b577f0bfdf5c047b22
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-07-20 21:40:04 +0200

    TempButton show which one is active

--- src/TempButton.js
+++ src/TempButton.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TempButton.css";
 
 export default function TempButton() {
-  return (
-    <div className="tempOption">
-      <span className="tempCelsius active">ºC</span>
-      <span className="separation"> | </span>
-      <span className="tempFahrenheit">ºF</span>
-    </div>
-  );
+  let [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="tempOption">
+        <span className="tempCelsius active">ºC</span>
+        <span className="separation"> | </span>
+        <span className="tempFahrenheit" onClick={showFahrenheit}>
+          ºF
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="tempOption">
+        <span className="tempCelsius" onClick={showCelsius}>
+          ºC
+        </span>
+        <span className="separation"> | </span>
+        <span className="tempFahrenheit active">ºF</span>
+      </div>
+    );
+  }
 }
