47cbbe1240b25d85a6dd7c87676813e39d6f3dab
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-07-23 14:50:13 +0200

    update forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
 import ForecastDays from "./ForecastDays";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   let days = [
     "Sunday",
     "Monday",
@@ -16,50 +19,31 @@ export default function Forecast(props) {
   let day = days[props.data.date.getDay()];
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data.list);
+    setForecast(response.data.list);
+    setLoaded(true);
   }
 
-  let apiKey = "a68381d4faf2a13b11b7dc8945964fc7";
-  let lat = props.coordinates.lat;
-  let lon = props.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <ForecastDays
+          day={day}
+          unit="C"
+          minValue={Math.round(forecast[0].main.temp_min)}
+          maxValue={Math.round(forecast[0].main.temp_max)}
+          icon={props.data.icon}
+          description={props.data.description}
+        />
+      </div>
+    );
+  } else {
+    let apiKey = "a68381d4faf2a13b11b7dc8945964fc7";
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <ForecastDays
-        day={day}
-        unit="C"
-        minValue={props.data.tempMin}
-        maxValue={props.data.tempMax}
-        icon={props.data.icon}
-        description={props.data.description}
-      />
-      <ForecastDays
-        day={day}
-        unit="C"
-        minValue={props.data.tempMin}
-        maxValue={props.data.tempMax}
-        icon={props.data.icon}
-        description={props.data.description}
-      />
-      <ForecastDays
-        day={day}
-        unit="C"
-        minValue={props.data.tempMin}
-        maxValue={props.data.tempMax}
-        icon={props.data.icon}
-        description={props.data.description}
-      />
-      <ForecastDays
-        day={day}
-        unit="C"
-        minValue={props.data.tempMin}
-        maxValue={props.data.tempMax}
-        icon={props.data.icon}
-        description={props.data.description}
-      />
-    </div>
-  );
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
